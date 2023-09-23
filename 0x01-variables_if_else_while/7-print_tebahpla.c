5d15e49af9744f4cb35cea6b4dc953e3aafd1f0e
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-23 11:35:27 -0700

    jjj

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+* main - this is main
+* Return: 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char x = 'z';
+
+	while   (x >= 'a')
+{
+	putchar(x);
+	x--;
+}
+putchar('\n');
+return (0);
+}
+
