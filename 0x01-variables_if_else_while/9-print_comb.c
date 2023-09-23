0caa56f710ebcb6b6888695ea6ce724369929673
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-23 12:37:50 -0700

    hgfhgf

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 {
 	int x = 0;
 	
-	while (x <= 8)
+	if (x <= 9)
 	{
 	putchar('0' + x);
-	x++;
+	if (x != 9)
+	{	
 	putchar(',');
 	putchar(' ');
 	}
-	putchar('9');
+	x++;
+	}
 return (0);
 }
