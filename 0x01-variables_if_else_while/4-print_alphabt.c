ac74d5b95f8ca8af274397684ada26c448ca5206
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-23 07:09:08 -0700

    kj

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+* main - this is main
+* Return: 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+{
+	if (x != 'q' && x != 'e')
+	{
+	putchar(x);
+	}
+	x++;
+}
+putchar('\n');
+return (0);
+}
+
