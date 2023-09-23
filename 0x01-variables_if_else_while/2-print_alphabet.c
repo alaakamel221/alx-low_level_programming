95a19ec7fa83298f7e18a0858f95d535f5ff7d58
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-23 06:45:49 -0700

    jgf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+* main - this is main
+* Return: 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char x = 'a';
+
+	while	(x <= 'z')
+{
+	putchar(x);
+	putchar('\n');
+	x++;
+}
+
+return (0);
+}
