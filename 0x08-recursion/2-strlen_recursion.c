3703a8dacca59828d7c42e5d2e3bac3c3820570d
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-30 13:47:32 -0700

    gggg

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,18 +3,17 @@
 /**
 * _strlen_recursion - is int  func
 * @s: is pointer
-* Return: 0
+* Return: int
 */
 
 int _strlen_recursion(char *s)
 	{
-		char i = '0';
+		int i = 0;
 
 		if (*s != '\0')
 		{
 		i++;
 		_strlen_recursion(s + 1);
 		}
-		putchar(i);
-	return (0);
+	return (i);
 	}
