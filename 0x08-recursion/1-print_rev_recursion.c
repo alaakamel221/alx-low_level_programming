b6c5ef090501b0318a5dd36db17c717c1cef56ee
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-09-30 13:08:32 -0700

    kkk

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,15 @@
 
 /**
 * _print_rev_recursion - like puts
+* @s: is pointer
 * Return: 0
 */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0' )
+	if (*s != '\0')
 	{
 	_print_rev_recursion(s + 1);
 	putchar(*s);
 	}
-	else
-	putchar('\n');
 }
