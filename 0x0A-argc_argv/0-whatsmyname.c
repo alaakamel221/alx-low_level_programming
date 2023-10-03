8f6146a39b40ccdad69ebf5a651e6853726320b0
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-03 13:31:25 -0700

    jjj

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+/**
+* main - is function
+* @argc: is int
+* @argv:aray pointer
+* Return: 0
+*/
 
 int main(int argc, char **argv)
 {
 	(void)argc;
-printf("%s\n", argv[0]);
+
+	printf("%s\n", argv[0]);
 return (0);
 }
