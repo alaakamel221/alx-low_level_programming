8fb3f4ab94d96fa3966517c4ede2324bf264ba88
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-03 13:52:19 -0700

    kkk

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main - is function
+* @argc: is int
+* @argv: is pointer
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+	}
