23aa79b969d1e0c4d15eea84286f40722847816a
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-03 14:51:34 -0700

    fff

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main - is function
+* @argc: is int
+* @argv: is p
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	printf("Error\n");
+	return (0);
+	}
