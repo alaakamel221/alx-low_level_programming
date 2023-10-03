5a91fa7080de1bba9697b86f46201700dd1413a7
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-03 15:32:48 -0700

    kkk

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - is fin
+* @argv: is p
+* @argc: is int
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+	for (c = argv[argc]; *c; c++)
+	if (*c < '0' || *c > '9')
+	{
+	return (printf("Error\n"),1);
+	}
+	sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
