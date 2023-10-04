2f921e24b561175b123b212ecef5215361c9160d
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-04 14:33:32 -0700

    jjj

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* _SAMPLE_HEADER_H_ */
