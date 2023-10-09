4e0cfbfdd9f0b0682ab9eceb5cf1b53c1615f89d
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-09 15:24:56 -0700

    ggg

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, x, v, d;
 
 	if (s1 == NULL)
-	s1 = " ";
+	s1 = "";
 	if (s2 == NULL)
-	s2 = " ";
+	s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	;
 	for (x = 0; s2[x] != '\0'; x++)
