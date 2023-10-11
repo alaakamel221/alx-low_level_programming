8910778f2172407f522b71f8c0bccd3e665235bd
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-11 16:07:39 -0700

    vv

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* print_dog - is fun
+* @d: int
+*/
+void print_dog(struct dog *d)
+	{
+		if (d)
+		{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+	}
