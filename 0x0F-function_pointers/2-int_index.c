68287cf8b263ff0afc89a52d71f399cb25172503
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-24 16:14:34 -0700

    ff

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+* int_index - is function
+* @array: is array
+* @size: int
+* @cmp: is function pointer
+* Return: 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	if(array && size && cmp)
+		while (i < size)
+		{
+		if (cmp(array[i]))
+		return (i);
+		i++;
+		}
+	return (-1);
+}
+
