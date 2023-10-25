3f5b2525ec1bf5d2f15691f39e2c04fd631eb763
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-24 17:08:45 -0700

    hhh

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+* op_add - is fun
+* @a: int
+* @b: int
+* Return: a+b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - is fun
+* @a: int
+* @b: int
+* Return: a-b
+*/
+
+int op_sub(int a, int b)
+{
+	retun (a - b);
+}
+
+/**
+* op_mul - is fun
+* @a: int
+* @b: int
+* Return: ab
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - is fun
+* @a: int
+* @b: int
+* Return: a/b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - is fun
+* @a: int
+* @b: int
+* Return: a%b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
