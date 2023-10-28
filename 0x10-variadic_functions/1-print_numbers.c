798663c99a0f5109bae76d644f3022a28b7fe5ef
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-28 06:13:06 -0700

    fff

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - is fun
+* @separator: ptr
+* @n: const int
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+	{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			printf("%s ", separator);
+		}
+		va_end(args);
+	printf("\n");
+	}
+	}
