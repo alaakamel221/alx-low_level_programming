bb7b2deed9e65e707dab7f596cd52e37ce200de9
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-29 11:50:17 -0700

    fff

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _str_length - to calculate length
+ * @s: char ptr
+ * Return: int
+ */
+int _str_length(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+while(*s++)
+       i++;
+return (i);
+}
+
+/**
+ * print_list - fun
+ * @h: list
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+	{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", _str_length(h->str), h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+i++;
+	}
+	return (i);
+	}
