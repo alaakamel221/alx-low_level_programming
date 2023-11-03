7a4d43c45aa04c8d29472c4f812289f5175d8f15
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-11-03 12:41:54 -0700

    gff

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * get_bit - fun
+ * @n: int
+ * @index: int
+ *Return: 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = sizeof(n) * 8, i = index;
+	int printed = 0;
+
+	if (!bit)
+		return (-1);
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			if (bit == i)
+				return (1);
+			printed++;
+		}
+		else if (printed && i == bit)
+			return (0);
+	}
+	if (!printed)
+		return (0);
+return (-1);
+}
