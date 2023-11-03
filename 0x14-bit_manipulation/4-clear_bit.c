21ca2670f93d2922dbe64e89410869d16122eb80
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-11-03 14:36:06 -0700

    fff

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,6 +4,7 @@
  * clear_bit - fun
  * @n: int ptr
  * @index: int
+ * Return: 0
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -11,6 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(n) * 8))
 		return (-1);
 	if (*n & 1L << index)
-	       *n = *n ^ (1 << index);
+		*n = *n ^ (1 << index);
 return (1);
 }
