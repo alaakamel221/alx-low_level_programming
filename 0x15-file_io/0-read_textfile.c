57ab880557b21047965316aac1c8616f6ba554d9
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-11-05 15:32:24 -0800

    hhh

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - is func
+ * @filename: char ptr to file name
+ * @letters: numb of letters to write
+ * Return: ssize_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buf[READ_BUF_SIZE];
+	size_t bytes;
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+
+	close(fd);
+	return (bytes);
+}
